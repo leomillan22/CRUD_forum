d9b3d1e72ca579e27cef8359ba3b9da1cc50c946
Leomillan <leonardo.amillan@outlook.com>
2022-08-22 22:34:21 -0300

    Post Model and Controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,21 @@
 var postModel = require("../models/postModel");
 
+function criaPost (req, res){
+    var titulo = req.body.tituloPost;
+    var conteudo = req.body.conteudoPost;
 
-
-
-module.exports = {};
+    postModel.criaPost(titulo, conteudo).then(
+        function(resultado){
+            res.json(resultado);
+        }
+    ).catch (
+        function(error){
+        console.log(error);
+        console.log("Erro ao cadastrar post. Erro ", error.sqlMessage);
+        res.status(500).json(error.sqlMessage);
+        }
+    )
+};
+module.exports = {
+    criaPost
+};
--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,5 +1,11 @@
 var database = require("../database/config");
 
+function criaPost(titulo, conteudo){
+    var instrucao = `INSERT INTO post(titulo, conteudo) VALUES('${titulo}', '${conteudo}')`;
 
+    return database.executar(instrucao);
+}
 
-module.exports = {};
+module.exports = {
+    criaPost
+};
